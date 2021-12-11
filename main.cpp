a763e1fec3cdac483b09ade7d79b616a651aa7a9
Tal Jacob <taljacob2@gmail.com>
2021-12-12 01:30:56 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * @mainpage mivnei_netunim targil tichnuti 2
+ *
+ * @author Tal Yacob, ID: 208632778.
+ * @version 1.0
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
