334af67737688be72f766993ffb87bafb60ff083
Tal Jacob <taljacob2@gmail.com>
2021-12-16 23:55:49 +0200

    after Array.h and StaticArray.h

--- Array.h
+++ Array.h
@@ -21,19 +21,34 @@
  */
 template<typename E> class Array {
 
-  private:
+  protected:
+    static constexpr char *PHYSICAL_SIZE_MESSAGE =
+            (char *) "Array: `physicalSize` must be at least `1`.";
+
+  protected:
+    static constexpr char *OUT_OF_RANGE_MESSAGE =
+            (char *) "Array: out of range.";
+
+  protected:
+    static constexpr char *ELEMENT_IS_NULL_MESSAGE =
+            (char *) "Array: Element is `nullptr`.";
+
+  protected:
     E **_array = nullptr;
 
-  private:
-    unsigned long _size = 0;
+  protected:
+    unsigned long _physicalSize = 100;
+
+  protected:
+    unsigned long getPhysicalSize() const { return _physicalSize; }
 
   public:
-    unsigned long getSize() const { return _size; }
-
-  public:
-    explicit Array(unsigned long size) {
-        _size  = size;
-        _array = new E *[_size];
+    explicit Array(unsigned long physicalSize) {
+        if (physicalSize < 1) {
+            throw std::invalid_argument(PHYSICAL_SIZE_MESSAGE);
+        }
+        _physicalSize = physicalSize;
+        _array        = new E *[_physicalSize];
     }
 
   public:
@@ -42,14 +57,48 @@ template<typename E> class Array {
   public:
     virtual ~Array() { deleteThis(); }
 
-  private:
+  protected:
     void deleteThis() { delete[] _array; }
 
   public:
-    E &operator[](unsigned long i) { return _array[i]; }
+    virtual E &getElement(unsigned long index) {
+        if (isOutOfRange(index)) {
+            throw std::out_of_range(OUT_OF_RANGE_MESSAGE);
+        }
+
+        E *pElement = _array[index];
+
+        if (pElement == nullptr) {
+            std::string msg = (char *) ELEMENT_IS_NULL_MESSAGE;
+            std::string msg2 =
+                    (char *) "Use the `getElementAsPointer(unsigned long)`"
+                             " method to retrieve it, instead of this"
+                             " method.";
+            throw std::runtime_error(msg + " " + msg2);
+        }
+
+        return *pElement;
+    }
 
   public:
-    const E &operator[](unsigned long i) const { return _array[i]; }
+    /**
+     * @note In case there are `nullptr`s in the array, you may use this
+     *       method to retrieve them without an error.
+     */
+    virtual E *getElementAsPointer(unsigned long index) {
+        if (isOutOfRange(index)) {
+            throw std::out_of_range(OUT_OF_RANGE_MESSAGE);
+        }
+
+        return _array[index];
+    }
+
+  protected:
+    virtual bool isOutOfRange(unsigned long index) {
+
+        // `_physicalSize` *must* be at least 1.
+        return !((0 <= index) && (index < _physicalSize));
+    }
 
   public:
     /**
@@ -64,7 +113,7 @@ template<typename E> class Array {
      * @return `this` object. So that you may "chain" this method with another.
      */
     Array<E> &forEach(const std::function<void(E &)> &callBack) {
-        for (int i = 0; i < _size; i++) { callBack(_array[i]); }
+        for (int i = 0; i < _physicalSize; i++) { callBack(_array[i]); }
 
         return this;
     }
@@ -110,7 +159,7 @@ template<typename E> class Array {
         });
 
         E **newArray = new E *[newArraySize];
-        for (int i = 0; i < _size; i++) {
+        for (int i = 0; i < _physicalSize; i++) {
             E &element = _array[i];
             if (predicate(element)) {
                 newArray[i] = element;
@@ -138,24 +187,24 @@ template<typename E> class Array {
      *                    to another object of type `E2`.
      *                    Afterwards, the elements in `this` array would be
      *                    `deleted` from the array. And based on the given @p
-     *                    deleteFilteredElements parameter,
+     *                    deleteOriginalArrayElements parameter,
      *                    they may also be `deleted` from the heap.
      *                    @note You are suggested to use a "lambda" function
      *                          for this function - so that it will be
      *                          quicker for you to use this method.
-     * @param deleteFilteredElements set this parameter to `true` if the
-     *                               elements in `this` array are allocated
-     *                               via the heap, so that this method will
-     *                               `delete` them if they are filtered out.
-     *                               Else, it means you allocated the elements
-     *                               locally, and in this case set this
-     *                               parameter to `false`.
+     * @param deleteOriginalArrayElements set this parameter to `true` if the
+     *                                    elements in `this` array are allocated
+     *                                    via the heap, so that this method will
+     *                                    `delete` them if they are filtered out.
+     *                                    Else, it means you allocated the elements
+     *                                    locally, and in this case set this
+     *                                    parameter to `false`.
      * @return the array of type `E2` mapped by `this` array. This way, you
      *         may also "chain" this method with another.
      */
     template<typename E2>
     Array<E2> &map(const std::function<E2 &(E &)> &mapFunction,
-                   bool deleteFilteredElements = false) {
+                   bool deleteOriginalArrayElements = false) {
 
         /*
          * Must iterate over the array once.
@@ -164,8 +213,8 @@ template<typename E> class Array {
          * - `delete` elements from `_array`.
          */
 
-        Array<E2> e2Array(_size);
-        for (int i = 0; i < _size; i++) {
+        Array<E2> e2Array(_physicalSize);
+        for (int i = 0; i < _physicalSize; i++) {
             E &element = _array[i];
             e2Array[i] = mapFunction(element);
 
@@ -173,28 +222,54 @@ template<typename E> class Array {
              * This element should be filtered out from the array.
              * `delete` the element if the user required to.
              */
-            if (deleteFilteredElements) { delete element; }
+            if (deleteOriginalArrayElements) { delete element; }
         }
 
         deleteThis();
         return e2Array;
     }
 
-  private:
+  protected:
     /**
      * @brief Shallow-Copying `this` object.
      * @return a shallow-copy of `this` object.
      */
     Array<E> copy() {
-        Array<E> copyArray(_size);
-        for (int i = 0; i < _size; i++) {
+        Array<E> copyArray(_physicalSize);
+        for (int i = 0; i < _physicalSize; i++) {
             copyArray[i] = _array[i]; // Shallow-Copy the reference.
         }
 
         return copyArray;
     }
 
-  private:
+  public:
+    virtual void setElement(E &element, unsigned long index) {
+        if (isOutOfRange(index)) {
+            throw std::out_of_range(OUT_OF_RANGE_MESSAGE);
+        }
+
+        /*
+         * IMPORTANT: Polymorphic use.
+         * A "pointer ( = *)" is the parent-class of a "reference ( = &)".
+         */
+        E *pElement         = &element;
+        this->_array[index] = pElement;
+    }
+
+  public:
+    /**
+     * @warning Use with caution.
+     */
+    virtual void setNull(unsigned long index) {
+        if (isOutOfRange(index)) {
+            throw std::out_of_range(OUT_OF_RANGE_MESSAGE);
+        }
+
+        this->_array[index] = nullptr;
+    }
+
+  protected:
     /**
      * @brief Update the fields of `this` object.
      * @param newArraySize .
@@ -202,17 +277,38 @@ template<typename E> class Array {
      */
     void update(unsigned long newArraySize, E **newArray) {
         deleteThis();
-        _array = newArray;
-        _size  = newArraySize;
+        _array        = newArray;
+        _physicalSize = newArraySize;
     }
 
   public:
     friend std::ostream &operator<<(std::ostream &os, const Array &array) {
+        return print(os, array);
+    }
+
+  private:
+    /**
+     * @note This method is `nullptr` resistant - Instead of crashing, it prints
+     *       "nullptr".
+     */
+    static std::ostream &print(std::ostream &os, const Array &array) {
         os << '[';
-        if (array._size) { os << array[0]; }
-        for (int i = 1; i < array._size; i++) { os << " ," << array[i]; }
+        if (array._physicalSize) { printElement(os, array._array[0]); }
+        for (unsigned long i = 1; i < array._physicalSize; i++) {
+            os << " ,";
+            printElement(os, array._array[i]);
+        }
         os << ']';
         return os;
+    }
+
+  protected:
+    static void printElement(std::ostream &os, const E *element) {
+        if (element == nullptr) {
+            os << "nullptr";
+        } else {
+            os << *element;
+        }
     }
 };
 
---
+++ StaticArray.h
@@ -0,0 +1,72 @@
+
+#ifndef STATIC_ARRAY_H
+#define STATIC_ARRAY_H
+
+#include "Array.h"
+
+/**
+ * @tparam E the type of `element` in the array.
+ * @version 1.0
+ */
+template<typename E> class StaticArray : public Array<E> {
+
+  protected:
+    static constexpr char *IS_EMPTY_MESSAGE =
+            (char *) "StaticArray: array is empty.";
+
+  protected:
+    static constexpr char *IS_FULL_MESSAGE =
+            (char *) "StaticArray: array is full.";
+
+  public:
+    explicit StaticArray(unsigned long physicalSize) : Array<E>(physicalSize) {}
+
+  protected:
+    unsigned long _logicalSize = 0;
+
+  public:
+    void push(E &element) {
+        if (_logicalSize == this->_physicalSize) {
+            throw std::runtime_error(IS_FULL_MESSAGE);
+        }
+
+        // May throw here.
+        this->setElement(element, _logicalSize);
+        _logicalSize++;
+    }
+
+  public:
+    void pushNull() {
+        if (_logicalSize == this->_physicalSize) {
+            throw std::runtime_error(IS_FULL_MESSAGE);
+        }
+
+        // May throw here.
+        this->setNull(_logicalSize);
+        _logicalSize++;
+    }
+
+  public:
+    E &pop() {
+        if (!_logicalSize) { throw std::runtime_error(IS_EMPTY_MESSAGE); }
+
+        // May throw here.
+        E& returnValue = this->getElement(_logicalSize - 1);
+        _logicalSize--;
+
+        return returnValue;
+    }
+
+  public:
+    E *popAsPointer() {
+        if (!_logicalSize) { throw std::runtime_error(IS_EMPTY_MESSAGE); }
+
+        // May throw here.
+        E *returnValue = this->getElementAsPointer(_logicalSize - 1);
+        _logicalSize--;
+
+        return this->getElementAsPointer(returnValue);
+    }
+};
+
+#endif // STATIC_ARRAY_H
