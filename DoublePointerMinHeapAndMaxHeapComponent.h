22a4a106cc1a7e163bd4f43e30ead0d7473e1864
Tal Jacob <taljacob2@gmail.com>
2021-12-17 19:31:04 +0200

    after DoublePointerMinHeapAndMaxHeapComponent insert

--- DoublePointerMinHeapAndMaxHeapComponent.h
+++ DoublePointerMinHeapAndMaxHeapComponent.h
@@ -2,11 +2,10 @@
 #ifndef DOUBLE_POINTER_MIN_HEAP_AND_MAX_HEAP_COMPONENT_H
 #define DOUBLE_POINTER_MIN_HEAP_AND_MAX_HEAP_COMPONENT_H
 
-#include "MinHeapWhenAlsoHavingMaxHeap.h"
 #include "MaxHeapWhenAlsoHavingMinHeap.h"
+#include "MinHeapWhenAlsoHavingMaxHeap.h"
 
-template<typename E>
-class DoublePointerMinHeapAndMaxHeapComponent {
+template<typename E> class DoublePointerMinHeapAndMaxHeapComponent {
 
   protected:
     MinHeapWhenAlsoHavingMaxHeap<E> minHeap;
@@ -14,6 +13,30 @@ class DoublePointerMinHeapAndMaxHeapComponent {
   protected:
     MaxHeapWhenAlsoHavingMinHeap<E> maxHeap;
 
+  public:
+    DoublePointerMinHeapAndMaxHeapComponent(
+            const MinHeapWhenAlsoHavingMaxHeap<E> &minHeap,
+            const MaxHeapWhenAlsoHavingMinHeap<E> &maxHeap)
+        : minHeap(minHeap), maxHeap(maxHeap) {}
+
+  public:
+    virtual ~DoublePointerMinHeapAndMaxHeapComponent() = default;
+
+  public:
+    const MinHeapWhenAlsoHavingMaxHeap<E> &getMinHeap() const {
+        return minHeap;
+    }
+
+  public:
+    const MaxHeapWhenAlsoHavingMinHeap<E> &getMaxHeap() const {
+        return maxHeap;
+    }
+
+  public:
+    void insert(E *element) {
+        minHeap.insert(element);
+        maxHeap.insert(element);
+    }
 };
 
 #endif // DOUBLE_POINTER_MIN_HEAP_AND_MAX_HEAP_COMPONENT_H
