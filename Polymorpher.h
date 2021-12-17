d3208458474bfa82ca6344311f8ea0c130da4fac
Tal Jacob <taljacob2@gmail.com>
2021-12-17 13:58:59 +0200

    after Polymorpher.h

--- Polymorpher.h
+++ Polymorpher.h
@@ -30,6 +30,15 @@ class Polymorpher {
     }
 
   public:
+    /**
+     * For example:
+     * @code
+     * auto minHeap = MinHeap<int, std::string>(1);
+     * HeapAdt<int, std::string> &heap =
+     *         Polymorpher::polymorph<HeapAdt<int, std::string>,
+     *                 MinHeap<int, std::string>>(minHeap);
+     * @endcode
+     */
     template<typename BASE_CLASS, typename SUB_CLASS>
     static BASE_CLASS &polymorph(const SUB_CLASS &subClass) {
         return (BASE_CLASS &) subClass;
