2a61e3d14b0c846c88976ad21fe1428c63c80de1
Tal Jacob <taljacob2@gmail.com>
2021-12-17 15:10:46 +0200

    back to working on main 3

--- Polymorpher.h
+++ Polymorpher.h
@@ -10,18 +10,15 @@ class Polymorpher {
      * @code
      * auto  minHeap = MinHeap<int, std::string>(1);
      * auto &heap    = Polymorpher::polymorphLValue<HeapAdt<int, std::string>,
-     *                                           MinHeap<int, std::string>>(
-     *         minHeap);
+     *                                              MinHeap<int, std::string>>(minHeap);
      * @endcode
      *
      * Another example:
      * @code
      * auto  priorityQueue = PriorityQueue<int, std::string>();
      * auto &priorityQueueAdt =
-     *         Polymorpher::polymorphLValue<PriorityQueueAdt<int,
-     *           std::string>,
-     *                                      PriorityQueue<int, std::string>>(
-     *                 priorityQueue);
+     *         Polymorpher::polymorphLValue<PriorityQueueAdt<int, std::string>,
+     *                                      PriorityQueue<int, std::string>>(priorityQueue);
      * @endcode
      */
     template<typename BASE_CLASS, typename SUB_CLASS>
