a0722b0847eb1f45ebe4cb3ecee6402ad2fbe30f
Tal Jacob <taljacob2@gmail.com>
2021-12-23 20:47:55 +0200

    after Unique doc

--- Unique.h
+++ Unique.h
@@ -5,6 +5,23 @@
 #include "Object.h"
 #include <iostream>
 
+/**
+ * @brief This special class wraps an *any* pointer of an element and
+ *        `delete`s it only if it was not allocated with the `new` operator.
+ *        This class is able to wrap a given "rvalue" to a pointer, via
+ *        heap allocation with the `new` operator - and knows to `delete` it
+ *        automatically when gets destructed.
+ *        This way, the user may create for example an *array* of `Unique`
+ *        elements, so that he will be able to "polymorph" "rvalues" with
+ *        "lvalues" together and not be bothered whether each specific
+ *        element should be manually `delete`d or not.
+ *
+ * @note Also, because this class is a sub-class of `Object`, then even if
+ *       the user *accidentally* `delete`d a "rvalue" `Unique` - then no
+ *       exception will be thrown, and it will be okay memory-wise without
+ *       any leaks.
+ * @tparam E the type of element to be stored.
+ */
 template<typename E> class Unique : public Object {
 
   protected:
@@ -14,12 +31,20 @@ template<typename E> class Unique : public Object {
     bool _needToDeleteElement = false;
 
   public:
+    /**
+     * @param element a "rvalue" element to be stored.
+     *                An extra allocation is needed.
+     */
     explicit Unique(E &&element) {
         _needToDeleteElement = true;
         _element             = new E(element);
     }
 
   public:
+    /**
+     * @param element a "lvalue" element to be stored.
+     *                No extra allocation is needed.
+     */
     explicit Unique(E *element) : _element(element) {}
 
   public:
